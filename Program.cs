4b08dff6cf70b430e942a4ca16d34fe981a4ec5b
Robson Rodrigues da Silva <robson_rodriguessilva@outlook.com>
2021-10-26 22:11:06 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿System.Diagnostics.Process process = new System.Diagnostics.Process();
+System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+startInfo.FileName = "powershell.exe";
+startInfo.Arguments = "cd $env:userprofile/Desktop; mkdir TestFolder";
+startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+process.StartInfo = startInfo;
+process.Start();
